f6f4c5589dded34914c46dc5696d12dfdcbac891
LAB24_USER ON WS33 <L24X15W33@LabHCI.net>
2026-09-14 17:26:58 +0800

    made formating changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,36 @@
 function App() {
 
   return (
-    <div className="py-10">
-      <h1 className="font-bold text-3xl px-8 text-blue-950">
+    <div className="py-15 px-30">
+      <h1 className="text-4xl px-8 py-4 font-medium">
         Ivan Ryle Aquino
       </h1>
       <p className="px-8">
         Fourth year BSIT student at Cebu Institute of Technology – University.
       </p>
       <hr className="mt-8 w-150 mx-8 border-slate-300" />
-      <h2 className="font-bold text-xl py-4 px-8 text-blue-950">
+      <h2 className="font-medium text-xl py-4 px-8">
         About
       </h2>
-      <p className="py-2 px-8 text-blue-950 justify-center">
+      <p className="py-2 px-8 justify-center">
         I live in Minglanilla, most of my time is spent watching anime or playing old video games. <br/>
         I only ever play non trendy games because I am of a contrarian mindset and dislike the popluace.<br/>
         The reason I chose BSIT is that I have a proficiency for it and loved playing around with <br/>
         computers ever since I was 3. Now I want to build my own games, and online experiences <br/>
         for people like me.
       </p>
-      <h2 className="font-bold text-xl py-4 px-8 text-blue-950">
+      <h2 className="font-medium text-xl py-4 px-8">
         Details
       </h2>
-      <p className="py-2 px-8 text-blue-950 justify-center">
+      <p className="py-2 px-8  justify-center">
         Course: BS Information Technology<br/>
         Year level: Fourth year<br/>
         Hometown: Minglanilla, Cebu<br/>
       </p>
-      <h2 className="font-bold text-xl py-4 px-8 text-blue-950">
+      <h2 className="font-medium text-xl py-4 px-8">
         Things I like:
       </h2>
-      <p className="py-2 px-8 text-blue-950 justify-center">
+      <p className="py-2 px-8  justify-center">
         <ul className="list-disc list-inside">
           <li>Older/Obscure video games</li>
           <li>Anime</li>
@@ -42,16 +42,19 @@ function App() {
           <li>Coding</li>
         </ul>
       </p>
-      <h2 className="font-bold text-xl py-4 px-8 text-blue-950">
+      <h2 className="font-medium text-xl py-4 px-8">
         Reach me
       </h2>
-      <p className="py-2 px-8 text-blue-950 justify-center">
+      <p className="py-2 px-8 justify-center">
         <ul className="list-disc list-inside">
           <li>Discord: kguardking</li>
           <li>e-mail: ivanrylevaquino@gmail.com</li>
           <li>phone no: +63 9695323015</li>
         </ul>
       </p>
+      <hr className="mt-8 w-150 mx-8 py-4 border-slate-300" />
+      <p classname="px-16  py-2 ">
+        Made for CSIT340.</p>
     </div>
   )
 }
